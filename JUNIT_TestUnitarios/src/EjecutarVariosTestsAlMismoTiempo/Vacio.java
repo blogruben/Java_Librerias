785b74f37432a2d667d4974dc96aeab247162830
Ruben <info@blogruben.com>
2023-04-19 09:18:30 +0200

    aniadir apachePoi y Junit

---
+++ JUNIT_TestUnitarios/src/EjecutarVariosTestsAlMismoTiempo/Vacio.java
@@ -0,0 +1,13 @@
+package EjecutarVariosTestsAlMismoTiempo;
+
+public class Vacio {
+
+	public static void main(String[] args) {
+		// JuNIT con espera, ejecutar varios al tiempo
+
+		
+		
+		
+	}
+
+}
---
+++ JUNIT_TestUnitarios/src/TestearConjuntoDeDatos/Main.java
@@ -0,0 +1,10 @@
+package TestearConjuntoDeDatos;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Apéndice de método generado automáticamente
+
+	}
+
+}
